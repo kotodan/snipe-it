18414f0dc89556ea8fc2eb0483d65c599f121a74
kotodan <arisond@gmail.com>
2021-07-21 09:43:07 +0300

    Actifs=Articles Actif=article select_company' => 'Sélectionnez une structure' Company=structure companie=structures companies=structure

--- resources/lang/fr/general.php
+++ resources/lang/fr/general.php
@@ -47,7 +47,7 @@
 	'click_here'			=> 'Cliquez ici',
     'clear_selection'		=> 'Effacer la sélection',
     'companies'			    => 'structures',
-    'company'				=> 'structure',
+    'company'				=> 'structures',
     'component'			    => 'Composant',
     'components'			=> 'Composants',
 	'complete'				=> 'Terminé',
