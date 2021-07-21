ab9d50b6e1fd5aaa1cc591336532ffee7d845eb9
kotodan <arisond@gmail.com>
2021-07-21 14:56:34 +0300

    Actifs=Articles Actif=article select_company' => 'Sélectionnez une structure' Company=Structure companie=structures companies=structure supplier=donateur créer des actifs=créer des articles série=Numero de série Emplacement par défaut=emplacement

--- resources/lang/fr/admin/categories/general.php
+++ resources/lang/fr/admin/categories/general.php
@@ -11,7 +11,7 @@ return array(
     'eula_text'							=> 'Catégorie de licence d\'utilisation',
     'eula_text_help'					=> 'Ce champ vous permet de configurer vos licences d\'utilisation pour chaque type d\'items. Si vous avez seulement une licence pour tout vos items, vous pouvez cochez la case ci-dessous pour utiliser celle par défaut.',
     'name'                              => 'Nom de la catégorie',
-    'require_acceptance'				=> 'L\'utilisateur doit confirmer qu\'il accepte les actifs de cette catégorie.',
+    'require_acceptance'				=> 'L\'utilisateur doit confirmer qu\'il accepte les articles de cette catégorie.',
     'required_acceptance'				=> 'L\'utilisateur recevra un courriel avec un lien de confirmation pour accepter ce produit.',
     'required_eula'						=> 'Cet utilisateur recevra par courriel une copie de la licence d\'utilisation',
     'no_default_eula'					=> 'Il n\'y a pas de licence d\'utilisation par défaut. Veuillez en ajouter une dans Préférences.',
